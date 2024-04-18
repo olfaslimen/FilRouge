56e1f03375e69d6ce92bad4136e28a409fabc533
olfaslimen <98602739+olfaslimen@users.noreply.github.com>
2024-04-18 09:21:01 +0200

    deploimenet test

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,17 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(notFound);
 app.use(errorHandler);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 app.listen(port, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
 );
